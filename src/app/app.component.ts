24e8ab8681db22a5237dc8a0bd2043deccddb1b9
belkoweb <belkoweb9718@gmail.com>
2019-11-11 22:21:34 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Book';
+
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyAv0EfCKcJ-PLmUO94TGhD8sSSTzOBRe_0",
+      authDomain: "book-87bb4.firebaseapp.com",
+      databaseURL: "https://book-87bb4.firebaseio.com",
+      projectId: "book-87bb4",
+      storageBucket: "gs://book-87bb4.appspot.com/",
+      messagingSenderId: "201029765837",
+      appId: "1:201029765837:web:1881fcb9f90be493"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthService} from './services/auth.service';
+import {BooksService} from './services/books.service';
+import {AuthGuardService} from './services/auth-guard.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    BookListComponent,
+    SingleBookComponent,
+    BookFormComponent,
+    HeaderComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, BooksService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
